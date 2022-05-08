63426844ec0c4bb0c4624bc94adf883d31a03cfd
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-07 22:52:53 -0300

    conectando ao backend utilizando axios

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { CircleNotch } from 'phosphor-react'
 
 export function Loading() {
   return (
-    <div className="w-5 h-6 flex justify-center items-center overflow-hidden">
+    <div className="flex items-center justify-center overflow-hidden">
       <CircleNotch weight="bold" className="w-4 h-4 animate-spin" />
     </div>      
   )
